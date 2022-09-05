bf259cd244c22c9ae19661c48ff50a2684887e13
Neo-Machina <andreistatache1997@gmail.com>
2022-09-05 19:24:35 +0200

    post resource show done

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <h1>List of Posts</h1>
+    <h1>Posts List</h1>
+
+    <div class="row row-cols-3">
+        @foreach ($posts as $post)
+            <div class="col mb-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h3 class="card-title">{{ $post->title }}</h3>
+                        <a href="{{ route('admin.posts.show', ['post' => $post->id]) }}" class="btn btn-primary">Submit</a>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 @endsection
---
+++ resources/views/admin/posts/show.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.dashboard')
+
+@section('content')
+    <div class="card showed_card">
+        <div class="card-body">
+            <h1 class="card-title">{{ $post->title }}</h1>
+
+            <div>
+                <span class="bold_text">Created at:</span> {{ $post->created_at }}
+            </div>
+
+            <div>
+                <span class="bold_text">Updated at:</span> {{ $post->updated_at }}
+            </div>
+
+            <div>
+                <span class="bold_text">Slug:</span> {{ $post->slug }}
+            </div>
+
+            <p class="card-text mt-5">
+                <span class="bold_text">Text:</span> {{ $post->content }}
+            </p>
+        </div>
+    </div>
+@endsection
